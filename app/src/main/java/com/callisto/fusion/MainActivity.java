b7f0090f2cfd5ae52355e343d7673a96409cdc24
Brook Greening <brookgreening@rover-207-205.rovernet.mtu.edu>
2018-03-01 23:02:18 -0500

    Layout changes, added button, tasks displayed

--- app/src/main/java/com/callisto/fusion/MainActivity.java
+++ app/src/main/java/com/callisto/fusion/MainActivity.java
@@ -1,13 +1,42 @@
 package com.callisto.fusion;
 
+import android.provider.MediaStore;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    String[] taskArray = new String[100];
+    int i =0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText(taskArray[0]);
+        final EditText addTaskText = (EditText) findViewById(R.id.addTaskText);
+
+        FloatingActionButton addTask = (FloatingActionButton) findViewById(R.id.floatingAddTask);
+        addTask.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                taskArray[i] = addTaskText.getText().toString();
+                i++;
+                textView.setText( textView.getText() + "   " + addTaskText.getText().toString());
+
+                addTaskText.setText("Add Task");
+
+            }
+                                       });
+
+
     }
 }
