c76a5a4e52395c4f7249d85dc0e43724d7b63d04
mpleclai <mpleclai@mtu.edu>
2018-03-01 22:58:31 -0500

    Added Navigation Drawer class and xml files as well as put the nav drawer button on the action bar at the top of the screen.

--- app/src/main/java/com/callisto/fusion/MainActivity.java
+++ app/src/main/java/com/callisto/fusion/MainActivity.java
@@ -1,13 +1,54 @@
 package com.callisto.fusion;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v4.widget.DrawerLayout;
 import android.os.Bundle;
+import android.view.View;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+
+//        mDrawerLayout.addDrawerListener(
+//                new DrawerLayout.DrawerListener() {
+//                    @Override
+//                    public void onDrawerSlide(@NonNull View drawerView, float slideOffset) {
+//
+//                    }
+//
+//                    @Override
+//                    public void onDrawerOpened(@NonNull View drawerView) {
+//
+//                    }
+//
+//                    @Override
+//                    public void onDrawerClosed(@NonNull View drawerView) {
+//                    }
+//
+//                    @Override
+//                    public void onDrawerStateChanged(int newState) {
+//
+//                    }
+//                }
+//        );
     }
+
 }
