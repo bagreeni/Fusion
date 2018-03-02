e2bc1eb13506e3231c8eabf1ed647bcdba33e172
Paul Wrubel <pwrubel7@gmail.com>
2018-03-01 22:15:48 -0500

    Added basic database JUnit test

--- app/src/androidTest/java/com/callisto/fusion/FusionRoomDatabaseTest.java
+++ app/src/androidTest/java/com/callisto/fusion/FusionRoomDatabaseTest.java
@@ -1,4 +1,19 @@
+package com.callisto.fusion;
+
+import android.arch.persistence.room.Room;
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.io.IOException;
+import java.util.List;
+
+import static junit.framework.Assert.assertEquals;
 
 /**
  * Created by voxaelfox on 3/1/18.
@@ -6,13 +21,15 @@ import org.junit.runner.RunWith;
 @RunWith(AndroidJUnit4.class)
 public class FusionRoomDatabaseTest {
     private TaskDAO mTaskDao;
+    private TaskTextDAO mTaskTextDao;
     private FusionDatabase mDb;
 
     @Before
     public void createDB() {
         Context context = InstrumentationRegistry.getTargetContext();
         mDb = Room.inMemoryDatabaseBuilder(context, FusionDatabase.class).build();
-        mUserDao = mDb.taskDAO();
+        mTaskDao = mDb.taskDAO();
+        mTaskTextDao = mDb.taskTextDAO();
     }
 
     @After
@@ -21,11 +38,19 @@ public class FusionRoomDatabaseTest {
     }
 
     @Test
-    public void writeUserAndReadInList() throws Exception {
-        Task task = ;
-        user.setName("george");
-        mUserDao.insert(user);
-        List<User> byName = mUserDao.findUsersByName("george");
-        assertThat(byName.get(0), equalTo(user));
+    public void createTaskAndCheck() throws Exception {
+        Task task = new Task();
+        task.taskID = 0;
+
+        TaskText textTask = new TaskText();
+        textTask.taskTextID = 0;
+        textTask.taskID = 0;
+        textTask.data = "Hello Task!";
+
+        mTaskDao.insert(task);
+        mTaskTextDao.insert(textTask);
+
+        List<TaskText> textTasks = mTaskTextDao.getAll();
+        assertEquals(textTasks.get(0).data, "Hello Task!");
     }
 }
