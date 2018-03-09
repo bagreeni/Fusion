e2cb19af93647f85439165bedf74e4f9b4b85440
Brook Greening <bagreeni@mtu.edu>
2018-03-08 22:22:29 -0500

    RecyclerView now has blocks for each task.

--- app/src/main/java/com/callisto/fusion/ui/RecyclerViewAdapter.java
+++ app/src/main/java/com/callisto/fusion/ui/RecyclerViewAdapter.java
@@ -1,8 +1,10 @@
 package com.callisto.fusion.ui;
 
 import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
@@ -25,8 +27,8 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     // you provide access to all the views for a data item in a view holder
     public static class ViewHolder extends RecyclerView.ViewHolder {
         // each data item is just a string in this case
-        public TextView mTextView;
-        public ViewHolder(TextView v) {
+        public View mTextView;
+        public ViewHolder(View v) {
             super(v);
             mTextView = v;
         }
@@ -39,10 +41,9 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        // create a new view
-//        TextView v = (TextView) LayoutInflater.from(parent.getContext())
-//                .inflate(R.layout.activity_main, parent, false);
-        TextView v = new TextView(parent.getContext());
+//         create a new view
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.recyclerview, parent, false);
         ViewHolder vh = new ViewHolder(v);
         return vh;
     }
@@ -51,7 +52,11 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         // - get element from your dataset at this position
         // - replace the contents of the view with that element
-        holder.mTextView.setText(data.get(position).data + data.get(position).categoryName);
+
+        ((TextView)holder.mTextView.findViewById(R.id.taskTitle)).setText(data.get(position).getData());
+        ((TextView)holder.mTextView.findViewById(R.id.taskCategory)).setText(data.get(position).getCategoryList());
+
+
 
     }
 
