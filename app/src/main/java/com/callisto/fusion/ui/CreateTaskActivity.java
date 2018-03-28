e04557beb0537d35e37b7bc16373314a23044151
mpleclai <mpleclai@mtu.edu>
2018-03-27 20:47:05 -0400

    added features to add task

--- app/src/main/java/com/callisto/fusion/ui/CreateTaskActivity.java
+++ app/src/main/java/com/callisto/fusion/ui/CreateTaskActivity.java
@@ -1,6 +1,8 @@
 package com.callisto.fusion.ui;
 
+import android.app.DatePickerDialog;
 import android.content.Context;
+import android.renderscript.RenderScript;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.ActionBar;
@@ -9,17 +11,24 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.DatePicker;
 import android.widget.EditText;
 
 import com.callisto.fusion.DataRepository;
 import com.callisto.fusion.R;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 public class CreateTaskActivity extends AppCompatActivity {
+
+    Calendar myCalendar;
+    EditText workDateBox, dueDateBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +42,44 @@ public class CreateTaskActivity extends AppCompatActivity {
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setHomeAsUpIndicator(R.drawable.ic_arrow_back);
 
+        myCalendar = Calendar.getInstance();
+        workDateBox = findViewById(R.id.work_date);
+        dueDateBox = findViewById(R.id.due_date);
+
+        final DatePickerDialog.OnDateSetListener dateWork = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, month);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabelWork();
+            }
+        };
+
+        final DatePickerDialog.OnDateSetListener dateDue = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, month);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabelDue();
+            }
+        };
+
+        workDateBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(CreateTaskActivity.this, dateWork ,myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH), myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+        dueDateBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(CreateTaskActivity.this, dateDue ,myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH), myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
         //create submit entries button on click listener to add all input to database on button press
         FloatingActionButton addTask = findViewById(R.id.submit_task_button);
     }
@@ -43,8 +90,11 @@ public class CreateTaskActivity extends AppCompatActivity {
         TextInputEditText taskTextBox = findViewById(R.id.task_name);
         TextInputEditText taskCategoryBox = findViewById(R.id.category_name);
 
+        TextInputEditText priorityBox = findViewById(R.id.priority);
+
         String taskText = taskTextBox.getText().toString();
         String rawCategoryList = taskCategoryBox.getText().toString();
+        //int priority = Integer.getInteger(priorityBox.getText().toString());
 
         String[] categoryArray = rawCategoryList.split(",");
 
@@ -61,6 +111,19 @@ public class CreateTaskActivity extends AppCompatActivity {
             InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
             imm.hideSoftInputFromWindow(view.getWindowToken(),0);
         }
+    }
+
+    private void updateLabelWork(){
+        String myFormat = "MM/dd/yy";
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        workDateBox.setText(sdf.format(myCalendar.getTime()));
+    }
+    private void updateLabelDue(){
+        String myFormat = "MM/dd/yy";
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        dueDateBox.setText(sdf.format(myCalendar.getTime()));
     }
 
 }
