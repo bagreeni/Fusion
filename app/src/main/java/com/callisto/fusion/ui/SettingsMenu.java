d9e3ab62369db40aa969f857965cc2cb290e913a
Brook Greening <bagreeni@mtu.edu>
2018-04-11 15:49:40 -0400

    settings menu changes

--- app/src/main/java/com/callisto/fusion/ui/SettingsMenu.java
+++ app/src/main/java/com/callisto/fusion/ui/SettingsMenu.java
@@ -1,11 +1,24 @@
 package com.callisto.fusion.ui;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Spinner;
+
+import com.callisto.fusion.R;
+
 /**
  * Created by brookgreening on 3/30/18.
  */
 
-public class SettingsMenu {
+public class SettingsMenu extends AppCompatActivity{
 
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_menu);
 
+        //Spinner colorSpinner = findViewById(R.id.spinner);
+
+    }
 
 }
